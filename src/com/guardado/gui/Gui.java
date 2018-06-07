e23add43f0e6af73795db4df63465921ae9cb880
UCA <00071114@uca.edu.sv>
2018-06-07 15:12:12 -0600

    Se hizo los clases

--- src/com/guardado/gui/Gui.java
+++ src/com/guardado/gui/Gui.java
@@ -12,6 +12,8 @@ public class Gui extends JFrame {
     private JPanel [] paneles;
     private JLabel [] labels;
     private JButton inicio;
+    private JButton reiniciar;
+    
     private String[] nombres = {"canguro","tortuga","dragon"};
 
     public Gui(){
@@ -26,6 +28,7 @@ public class Gui extends JFrame {
         paneles = new JPanel[3];
         labels = new JLabel[3];
         inicio = new JButton("Inicio");
+        reiniciar = new JButton("Reiniciar");
         Container container = getContentPane();
         //llenando el panel
         for (int i=0 ; i < 3 ; i++)
@@ -38,21 +41,43 @@ public class Gui extends JFrame {
             container.add(labels[i]);
         }
         inicio.setBounds(10,0,100,30);
+        //reiniciar.setBounds(130, 0, 150, 30);
         container.add(inicio);
+        //container.add(reiniciar);
         inicio.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                AnimalThread canguro = new AnimalThread("canguro",labels[0].getX(),labels[0].getY(),510,labels[0]);
-                AnimalThread tortuga = new AnimalThread("colibri",labels[1].getX(),labels[1].getY(),510,labels[1]);
-                AnimalThread dragon = new AnimalThread("dragon",labels[2].getX(),labels[2].getY(),510,labels[2]);
+                AnimalThread canguro = new AnimalThread("canguro",labels[0].getX(),labels[0].getY(),510,labels[0], reiniciar);
+                AnimalThread tortuga = new AnimalThread("tortuga",labels[1].getX(),labels[1].getY(),510,labels[1], reiniciar);
+                AnimalThread dragon = new AnimalThread("dragon",labels[2].getX(),labels[2].getY(),510,labels[2], reiniciar);
                 canguro.start();
                 tortuga.start();
                 dragon.start();
+                //container.add(reiniciar);
+                reiniciar.setVisible(false);
             }
         });
+        //setSize(700,650);
+        reiniciar.setBounds(130, 0, 150, 30);
+        container.add(reiniciar);
+        reiniciar.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                for (int i=0 ; i < 3 ; i++){
+                    labels[i].setBounds(10,(i*220)+10,200,200);
+                }
+                //reiniciar.setVisible(false);
+                inicio.setVisible(true);
+                reiniciar.setVisible(false);
+            }
+            
+        });
         setSize(700,650);
+     
+        
     }
-
+    
+        
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
--- src/com/guardado/threads/AnimalThread.java
+++ src/com/guardado/threads/AnimalThread.java
@@ -3,6 +3,7 @@ package com.guardado.threads;
 import javax.swing.*;
 
 public class AnimalThread extends Thread {
+    private JButton b;
     private String nombre;
     private int limite;
     private JLabel animal;
@@ -11,12 +12,13 @@ public class AnimalThread extends Thread {
 
     public AnimalThread(){}
 
-    public AnimalThread(String nombre, int x, int y, int limite, JLabel animal) {
+    public AnimalThread(String nombre, int x, int y, int limite, JLabel animal, JButton b) {
         this.nombre = nombre;
         this.x = x;
         this.y= y;
         this.limite = limite;
         this.animal = animal;
+        this.b=b;
     }
 
     @Override
@@ -27,12 +29,19 @@ public class AnimalThread extends Thread {
             System.out.println(this.nombre + " avanza");
             this.animal.setLocation(i,y);
             try {
-                sleep(100);
+                if(this.nombre=="tortuga")
+                    sleep(150);
+                else
+                    sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        if(nombre=="tortuga"){
+            b.setVisible(true);
+        }
         System.out.println(this.nombre + " a llegado a la meta");
+        
 
         yield();
     }
